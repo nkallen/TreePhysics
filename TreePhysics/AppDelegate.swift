98b0151b21e1b4a4f2357cf67b492766844d794f
Nick Kallen <nickkallen@me.com>
2019-07-16 12:20:31 +0200

    Initial Commit

---
+++ TreePhysics/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  TreePhysics
+//
+//  Created by Nick Kallen on 7/16/19.
+//  Copyright © 2019 Nick Kallen. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+}
