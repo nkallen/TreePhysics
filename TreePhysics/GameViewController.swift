291019a1003c4577fb36d906ce46ef6a20d7782c
Nick Kallen <nickkallen@me.com>
2019-07-17 12:38:33 +0200

    Drawing tree again

--- TreePhysics/GameViewController.swift
+++ TreePhysics/GameViewController.swift
@@ -21,34 +21,38 @@ class GameViewController: NSViewController {
         cameraNode.camera = SCNCamera()
         scene.rootNode.addChildNode(cameraNode)
         cameraNode.position = SCNVector3(x: 0, y: 1, z: 5)
+        cameraNode.name = "Camera"
         
         // create and add an ambient light to the scene
         let ambientLightNode = SCNNode()
         ambientLightNode.light = SCNLight()
         ambientLightNode.light!.type = .ambient
+        ambientLightNode.name = "Ambient Light"
         scene.rootNode.addChildNode(ambientLightNode)
         
-        // add the tree
-//        let root = Branch()
-//        let b1 = Branch()
-//        let b2 = Branch()
-
-//        root.add(b1)
-//        b1.add(b2)
-//        let tree = Tree(root)
-
-//        b2.apply(force: float2(-1,0), at: 1)
-//        tree.root.updateComposite()
-//        for branch in [tree.root, b1, b2] {
-//            print(branch.name, branch.compositeMass, branch.compositeForce, branch.compositeTorque)
-//        }
-
-//        scene.rootNode.addChildNode(tree.root.node)
-
-        let scnView = self.view as! SCNView
         scnView.scene = scene
         scnView.allowsCameraControl = true
         scnView.showsStatistics = true
         scnView.backgroundColor = NSColor.black
+    }
+
+    override func viewDidAppear() {
+//        add the tree
+        let root = Branch()
+        let b1 = Branch()
+        let b2 = Branch()
+
+        root.add(b1)
+        b1.add(b2)
+        let tree = Tree(root)
+
+        b2.apply(force: float2(0,1), at: 1)
+        tree.root.updateComposite()
+
+        scnView.scene!.rootNode.addChildNode(tree.root.node)
+    }
+
+    var scnView: SCNView {
+        return self.view as! SCNView
     }
 }
