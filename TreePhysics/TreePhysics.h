cac9a450768d74d75aa7dd81a98951afca9814f3
Nick Kallen <nickkallen@me.com>
2019-09-23 12:55:25 +0200

    missing files

---
+++ TreePhysics/TreePhysics.h
@@ -0,0 +1,19 @@
+//
+//  TreePhysics.h
+//  TreePhysics
+//
+//  Created by Nick Kallen on 9/11/19.
+//  Copyright © 2019 Nick Kallen. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for TreePhysics.
+FOUNDATION_EXPORT double TreePhysicsVersionNumber;
+
+//! Project version string for TreePhysics.
+FOUNDATION_EXPORT const unsigned char TreePhysicsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TreePhysics/PublicHeader.h>
+
+
