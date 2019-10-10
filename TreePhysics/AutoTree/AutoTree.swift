4d1f0d545fbdc2586d23069bd06e2d21c4d9187a
Nick Kallen <nickkallen@me.com>
2019-10-10 14:11:05 +0200

    extract config file

--- TreePhysics/AutoTree/AutoTree.swift
+++ TreePhysics/AutoTree/AutoTree.swift
@@ -3,45 +3,6 @@ import simd
 import SceneKit
 
 struct AutoTree {
-    struct Config {
-        var branchingAngle: Float = .pi/4
-        var phyllotacticAngle: Float = .pi/4
-        var internodeLength: Float = 0.05
-
-        var occupationRadius: Float = 0.05
-        var perceptionAngle: Float = .pi/4
-        var perceptionRadius: Float = 0.05 * 12
-
-        var meshDetail: Float = 1.1
-        var extremityRadius: Float = 0.001
-        var baseRadius: Float = 0.05
-
-        var fullExposure: Float = 10
-        var shadowIntensity: Float = 0.1
-        var sensitivityOfBudsToLight: Float = 1.1
-        var biasVigorTowardsMainAxis: Float = 0.5
-        var maxShootLength: Int = 3
-
-        func validate() {
-            check.angle(branchingAngle)
-            check.angle(phyllotacticAngle)
-            check.angle(perceptionAngle)
-
-            check.positive(internodeLength)
-            check.positive(occupationRadius)
-            check.positive(perceptionRadius)
-            check.assert(meshDetail > 1)
-            check.positive(extremityRadius)
-            check.positive(baseRadius)
-
-            check.positive(fullExposure)
-            check.positive(shadowIntensity)
-            check.nonneg(sensitivityOfBudsToLight)
-            check.normal(biasVigorTowardsMainAxis)
-            check.assert(maxShootLength >= 1)
-        }
-    }
-
     let config: Config
 
     init(_ config: Config = Config()) {
